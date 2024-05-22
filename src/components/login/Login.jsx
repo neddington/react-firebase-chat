19175630525ade783b15684ef48037b3049292cf
Nick Eddington <neddington@cascadia.edu>
2024-05-22 16:46:13 -0700

    Login Updates

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -6,7 +6,7 @@ import {
   signInWithEmailAndPassword,
 } from "firebase/auth";
 import { auth, db } from "../../lib/firebase";
-import { doc, setDoc } from "firebase/firestore";
+import { collection, doc, getDocs, query, setDoc, where } from "firebase/firestore";
 import upload from "../../lib/upload";
 
 const Login = () => {
@@ -32,6 +32,18 @@ const Login = () => {
     const formData = new FormData(e.target);
 
     const { username, email, password } = Object.fromEntries(formData);
+
+    if (!username || !email || !password)
+        return toast.warn("Please enter inputs!") && setLoading(false);
+      if (!avatar.file) return toast.warn("Please upload an avatar!") && setLoading(false);
+
+
+    const usersRef = collection(db, "users");
+    const q = query(usersRef, where("username", "==", username));
+    const querySnapshot = await getDocs(q);
+    if (!querySnapshot.empty) {
+      return toast.warn("Select another username");
+    }
 
     try {
       const res = await createUserWithEmailAndPassword(auth, email, password);
--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { Query, getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 
