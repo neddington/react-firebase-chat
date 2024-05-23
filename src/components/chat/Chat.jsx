8a38c5fb2f1a5e577024a3918a67482b357619e5
Nick Eddington <neddington@cascadia.edu>
2024-05-23 11:06:46 -0700

    Send new Chat

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,20 +1,89 @@
-import { useEffect, useRef, useState } from "react"
-import "./chat.css"
-import EmojiPicker from "emoji-picker-react"
+import { useEffect, useRef, useState } from "react";
+import "./chat.css";
+import EmojiPicker from "emoji-picker-react";
+import {
+    arrayUnion,
+    doc,
+    getDoc,
+    onSnapshot,
+    updateDoc,
+  } from "firebase/firestore";
+  import { db } from "../../lib/firebase";
+  import { useChatStore } from "../../lib/chatStore";
+  import { useUserStore } from "../../lib/userStore";
+import { get } from "firebase/database";
 
 const Chat = () => {
+    const [chat, setChat] = useState(); 
     const [open,setOpen] = useState(false)
     const [text,setText] = useState("")
+
+    const { currentUser } = useUserStore();
+    const { chatId, user, isCurrentUserBlocked, isReceiverBlocked } =
+      useChatStore()
 
     const endRef = useRef(null)
 
     useEffect(()=>{
         endRef.current?.scrollIntoView({behavior:"smooth"})
     },[])
+
+    useEffect(() => {
+        const unSub = onSnapshot(doc(db, "chats", chatId), (res) => {
+          setChat(res.data());
+        });
     
-    const handleEmoji = e =>{
+        return () => {
+          unSub();
+        };
+      }, [chatId]);
+    
+    const handleEmoji = (e) =>{
         setText(prev=>prev+e.emoji);
         setOpen(false)
+    };
+
+    const handleSend = async ()=>{
+        if(text === "") return;
+
+        try{
+
+            await updateDoc(doc(db, "chats", chatId),{
+                messages:arrayUnion({
+                    senderId: currentUser.id,
+                    text,
+                    createdAt: new Date(),
+
+                }),
+            });
+
+            const userIDs = [currentUser.id, user.id];
+
+            userIDs.forEach(async (id)=>{
+
+            
+            const userChatRef = doc(db,"userchats", id)
+            const userChatSnapshot = await getDoc(userChatRef)
+
+            if(userChatSnapshot.exists()){
+                const userChatsData = userChatSnapshot.data()
+
+                const chatIndex = userChatsData.chats.findIndex(c=> c.chatId === chatId)
+
+                userChatsData.chats[chatIndex].lastMessage = text;
+                userChatsData.chats[chatIndex].isSeen = 
+                    id === currentUser.id ? true : false;
+                userChatsData.chats[chatIndex].updatedAt = Date.now();
+
+                await updateDoc(userChatRef,{
+                    chats:userChatsData.chats,
+                });
+            }
+        });
+
+        }catch(err){
+            console.log(err)
+        }
     };
 
     return (
@@ -34,70 +103,19 @@ const Chat = () => {
                 </div>
             </div>
             <div className="center">
-                <div className="message">
-                    <img src="./avatar.png" alt="" />
+                {chat?.messages?.map((message) => (
+                <div className="message own" key={message.createAt}>
                     <div className="texts">
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-                            Voluptatibus cumque expedita modi quos blanditiis dolor omnis. 
-                            Nobis autem eos tempore provident similique quas, molestiae 
-                            commodi maxime modi architecto quo debitis.
+                        {message.img && <img 
+                            src={message.img} 
+                            alt="" 
+                        />}
+                        <p>{message.text}
                         </p>
-                        <span>1 min ago</span>
+                        {/*<span>{message.createAt}</span>*/}
                     </div>
                 </div>
-                <div className="message own">
-                    <div className="texts">
-                        <img src="https://images.pexels.com/photos/19155212/pexels-photo-19155212/free-photo-of-roof-on-a-yellow-building.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-                            Voluptatibus cumque expedita modi quos blanditiis dolor omnis. 
-                            Nobis autem eos tempore provident similique quas, molestiae 
-                            commodi maxime modi architecto quo debitis.
-                        </p>
-                        <span>1 min ago</span>
-                    </div>
-                </div>
-                <div className="message">
-                    <img src="./avatar.png" alt="" />
-                    <div className="texts">
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-                            Voluptatibus cumque expedita modi quos blanditiis dolor omnis. 
-                            Nobis autem eos tempore provident similique quas, molestiae 
-                            commodi maxime modi architecto quo debitis.
-                        </p>
-                        <span>1 min ago</span>
-                    </div>
-                </div>
-                <div className="message own">
-                    <div className="texts">
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-                            Voluptatibus cumque expedita modi quos blanditiis dolor omnis. 
-                            Nobis autem eos tempore provident similique quas, molestiae 
-                            commodi maxime modi architecto quo debitis.
-                        </p>
-                        <span>1 min ago</span>
-                    </div>
-                </div>
-                <div className="message">
-                    <img src="./avatar.png" alt="" />
-                    <div className="texts">
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-                            Voluptatibus cumque expedita modi quos blanditiis dolor omnis. 
-                            Nobis autem eos tempore provident similique quas, molestiae 
-                            commodi maxime modi architecto quo debitis.
-                        </p>
-                        <span>1 min ago</span>
-                    </div>
-                </div>
-                <div className="message own">
-                    <div className="texts">
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-                            Voluptatibus cumque expedita modi quos blanditiis dolor omnis. 
-                            Nobis autem eos tempore provident similique quas, molestiae 
-                            commodi maxime modi architecto quo debitis.
-                        </p>
-                        <span>1 min ago</span>
-                    </div>
-                </div>
+                ))}
                 <div ref={endRef}></div>
             </div>
             <div className="bottom">
@@ -121,7 +139,7 @@ const Chat = () => {
                     <EmojiPicker open={open} onEmojiClick={handleEmoji}/>
                     </div>
                 </div>
-                <button className="sendButton">Send</button>
+                <button className="sendButton" onClick={handleSend}>Send</button>
             </div>
         </div>
     )
